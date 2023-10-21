f3f5be1e065fabb700e6fd06626527d119a18d65
Md-Asikur <asikurrahaman997@gamil.com>
2023-10-22 03:23:11 +0600

    update

--- pages/index.js
+++ pages/index.js
@@ -7,14 +7,35 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Welcome Homes -Kaya</title>
-        <meta name="description" content="Welcome Homes -Kaya" />
+        <title>Welcome Homes - Kaya: Your Gateway to Dream Getaways</title>
+        <meta
+          name="description"
+          content="Discover a curated selection of exceptional homes with Welcome Homes - Kaya. Book your perfect stay and create lasting memories."
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <meta name="author" content="Kaya" /> {/* Add the author's name */}
+        <meta name="name" content="Welcome Homes - Kaya" /> {/* Add the web name */}
+        <meta name="robots" content="index, follow" />{" "}
+        {/* Direct Google bot to index the page */}
+        <meta name="googlebot" content="index, follow" /> {/* For Google bot */}
+        <meta
+          property="og:title"
+          content="Welcome Homes - Kaya: Your Gateway to Dream Getaways"
+        />
+        <meta
+          property="og:description"
+          content="Discover a curated selection of exceptional homes with Welcome Homes - Kaya. Book your perfect stay and create lasting memories."
+        />
+        <meta
+          property="og:image"
+          content="https://res.cloudinary.com/asikur/image/upload/v1697922761/Screenshot_510_zagdim.png"
+        />
+        <link rel="icon" href="/logo.jpeg" />
       </Head>
+
       <div className="container">
-        <MapComponent/>
+        <MapComponent />
       </div>
     </>
-  )
+  );
 }
