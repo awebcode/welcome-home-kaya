b7cc13e72431ee6a21ce38562c1566c41987584b
Coding Lover -Asikur <141431875+Md-Asikur-Rahman2@users.noreply.github.com>
2023-11-11 18:20:02 +0600

    Update CustomHouseCard.js

--- components/house_preview/customization/CustomHouseCard.js
+++ components/house_preview/customization/CustomHouseCard.js
@@ -74,14 +74,14 @@ const CustomHouseCard = ({ data, project }) => {
     <div className="min-w-max rounded-s mx-auto bg-white shadow-lg rounded-md overflow-hidden my-2">
       <div
         onClick={() => router.push(`/products/view/${data._id}`)}
-        className="relative h-32 rounded-md "
+        className="relative h-32 rounded-md cursor-pointer "
       >
         <Image src={data?.images[0]} alt={data?.title} layout="fill" objectFit="cover" />
       </div>
       <div className="p-4">
         <h2
           onClick={() => router.push(`/products/view/${data._id}`)}
-          className="text-gray-900 text-sm font-bold "
+          className="text-gray-900 text-sm font-bold cursor-pointer"
         >
           {data?.title}
         </h2>
